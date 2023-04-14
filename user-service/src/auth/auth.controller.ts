48d1db245e06c3a70d9e0fa3642ac495f38421e8
gao-m16 <gustavo.olmedo.formosa@gmail.com>
2023-04-14 12:46:59 +0100

    add auth module boilerplate & register dto

---
+++ user-service/src/auth/auth.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterDto } from './dtos/register.dto';
+
+@Controller('auth')
+export class AuthController {
+  @Post('register')
+  register(@Body() body: RegisterDto) {
+    return body;
+  }
+}
