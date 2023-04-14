28f218ad0721090290843646d007cf95c9ca47ee
gao-m16 <gustavo.olmedo.formosa@gmail.com>
2023-04-14 17:50:33 +0100

    add deps for connecting cart service to the db

--- cart-service/src/app.module.ts
+++ cart-service/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'cart-service-db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'kk_carts',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
