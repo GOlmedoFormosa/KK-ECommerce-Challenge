40102111bc8803cca75b9d35ec150b8a50066e35
gao-m16 <gustavo.olmedo.formosa@gmail.com>
2023-04-14 13:22:32 +0100

    add enable cors

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -9,6 +9,10 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3200', // URL of the client
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
