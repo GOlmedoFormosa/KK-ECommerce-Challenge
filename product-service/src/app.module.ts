d033a7c808294f7df7f1ff4943234d7a26b74382
gao-m16 <gustavo.olmedo.formosa@gmail.com>
2023-04-14 15:16:24 +0100

    add product module, controller and service nest boilerplate

--- product-service/src/app.module.ts
+++ product-service/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://product-service-db:27017')],
+  imports: [
+    MongooseModule.forRoot('mongodb://product-service-db:27017'),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
