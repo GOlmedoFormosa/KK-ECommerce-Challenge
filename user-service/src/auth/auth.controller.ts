154517834abba664717f39601510f129f4ec9c81
gao-m16 <gustavo.olmedo.formosa@gmail.com>
2023-04-14 12:55:23 +0100

    add create user service & add bcryptjs to hash password, and import user module into auth module to create user when we register

--- user-service/src/auth/auth.controller.ts
+++ user-service/src/auth/auth.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+
 import { RegisterDto } from './dtos/register.dto';
+import { UserService } from '../user/user.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private userService: UserService) {}
   @Post('register')
-  register(@Body() body: RegisterDto) {
-    return body;
+  async register(@Body() body: RegisterDto) {
+    const { password_confirm, ...data } = body;
+    if (body.password !== password_confirm) {
+      throw new BadRequestException('Password do not match!');
+    }
+    const passHashed = await bcrypt.hash(body.password, 12);
+    return this.userService.save({
+      ...data,
+      password: passHashed,
+    });
   }
 }
