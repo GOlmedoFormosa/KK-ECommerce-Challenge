2c2b93777b8291c87a2abb08b8fa8e44e5f3fc0a
gao-m16 <gustavo.olmedo.formosa@gmail.com>
2023-04-15 14:02:19 +0100

    add guard improvement & add tests for guard

--- product-service/src/auth/auth/auth.guard.spec.ts
+++ product-service/src/auth/auth/auth.guard.spec.ts
@@ -1,8 +1,75 @@
+import { createMock } from '@golevelup/ts-jest';
+import { Test, TestingModule } from '@nestjs/testing';
+import { faker } from '@faker-js/faker';
+import { randomInt } from 'crypto';
+import { ExecutionContext } from '@nestjs/common';
+
 import { AuthGuard } from './auth.guard';
+import { AuthService } from '../auth.service';
 
 describe('AuthGuard', () => {
+  let guard: AuthGuard;
+  let service: AuthService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            authorize: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<AuthService>(AuthService);
+    guard = new AuthGuard(service);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
-    // expect(new AuthGuard()).toBeDefined();
-    expect(true).toBe(true);
+    expect(guard).toBeDefined();
+  });
+
+  it('should return true with auth', async () => {
+    const context = createMock<ExecutionContext>();
+
+    context.switchToHttp().getRequest.mockReturnValue({
+      cookies: {
+        jwt: 'thisIsACookie',
+      },
+    });
+    const user = {
+      id: randomInt(10, 100),
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email(),
+    };
+    jest.spyOn(service, 'authorize').mockResolvedValue(user);
+    const res = await guard.canActivate(context);
+    expect(res).toBeTruthy();
+  });
+
+  it('should return false without cookie', async () => {
+    const context = createMock<ExecutionContext>();
+
+    context.switchToHttp().getRequest.mockReturnValue(undefined);
+    const res = await guard.canActivate(context);
+    expect(res).toBeFalsy();
+  });
+
+  it('should return false with an invalid credential', async () => {
+    const context = createMock<ExecutionContext>();
+
+    context.switchToHttp().getRequest.mockReturnValue({
+      cookies: {
+        jwt: 'thisIsACookie',
+      },
+    });
+    jest.spyOn(service, 'authorize').mockReturnValue(undefined);
+    const res = await guard.canActivate(context);
+    expect(res).toBeFalsy();
   });
 });
