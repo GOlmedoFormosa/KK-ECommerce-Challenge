4656b96b77b23f8ced2990e289dc2bd29b55a028
gao-m16 <gustavo.olmedo.formosa@gmail.com>
2023-04-15 14:13:30 +0100

    add auth service tests

--- product-service/src/auth/auth.service.spec.ts
+++ product-service/src/auth/auth.service.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpService } from '@nestjs/axios';
+import { of } from 'rxjs';
+import { faker } from '@faker-js/faker';
+import { randomInt } from 'crypto';
+
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  // let service: AuthService;
+  let service: AuthService;
+  let httpService: HttpService;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [AuthService],
-  //   }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
 
-  //   service = module.get<AuthService>(AuthService);
-  // });
+    service = module.get<AuthService>(AuthService);
+    httpService = module.get<HttpService>(HttpService);
+  });
 
   it('should be defined', () => {
-    expect(true).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  it('should return an authorized user', async () => {
+    const fakeUser = {
+      id: randomInt(10, 100),
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email(),
+    };
+
+    jest.spyOn(httpService, 'get').mockReturnValueOnce(
+      of({
+        data: fakeUser,
+        headers: {},
+        config: { url: 'http://user-service:3000/api/auth/authorize' },
+        status: 200,
+        statusText: 'OK',
+      }) as any,
+    );
+
+    const userRes = await service.authorize('asdf');
+    expect(userRes).toEqual(fakeUser);
   });
 });
